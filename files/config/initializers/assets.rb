88b16261951a81baf3454b39efa7359912c85e91
miks <miks@cubesystems.lv>
2015-02-16 15:26:52 +0200

    Fix assets initializer

--- files/config/initializers/assets.rb
+++ files/config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( *.css *.js *.eot *.svg *.ttf *.woff lib/* controllers/* 3rd_party/* )
+Rails.application.config.assets.precompile += %w(*.eot *.svg *.ttf *.woff)
