357264a76f49679947c1d5f7a038d38463b725a2
miks <miks@cubesystems.lv>
2015-05-07 21:50:35 +0300

    Do not link bin/ with capistrano

--- files/config/deploy.rb
+++ files/config/deploy.rb
@@ -2,7 +2,7 @@ set :application, '_application_name_'
 set :repo_url, '_repo_url_'
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :linked_files, %w{config/database.yml config/secrets.yml}
 
 after 'deploy:publishing', 'deploy:restart'
