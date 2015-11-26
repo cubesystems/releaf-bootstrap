168584d30f55d67c3280060f19567f3f15bfd78f
miks <miks@cubesystems.lv>
2015-11-26 11:19:30 +0200

    Reboot safe unicorn starting

--- files/config/schedule.rb
+++ files/config/schedule.rb
@@ -1,10 +1,12 @@
-job_type :unicorn_start, "cd :path && RAILS_ENV=:environment bundle exec unicorn_rails -c :path/config/unicorn/:environment.rb -D"
-job_type :logrotate,     "cd :path && RAILS_ENV=:environment logrotate -f -s :path/log/logrotate.state :path/config/logrotate.conf"
+job_type :unicorn_start, "cd #{@deploy_to}/current && RAILS_ENV=:environment bundle exec unicorn_rails -c #{@deploy_to}/current/config/unicorn/:environment.rb -D"
+job_type :logrotate,     "cd #{@deploy_to}/current && RAILS_ENV=:environment logrotate -f -s #{@deploy_to}/current/log/logrotate.state #{@deploy_to}/current/config/logrotate.conf"
+set :output, 'log/cron.log'
+
 
 every :reboot do
   unicorn_start ''
 end
 
-every :day, :at => '2:30am' do
+every :day, at: '2:30am' do
   logrotate ''
 end
