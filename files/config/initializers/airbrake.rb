8c683b11d5506bff2d78c4443ca3cac6277fc828
miks <miks@cubesystems.lv>
2016-01-26 00:18:49 +0200

    Update Airbrake initializer

--- files/config/initializers/airbrake.rb
+++ files/config/initializers/airbrake.rb
@@ -1,9 +1,20 @@
 Airbrake.configure do |config|
-  config.api_key		 	= '_error_report_key_'
-  config.host				= 'office.cube.lv'
-  config.port				= 80
-  config.secure			= config.port == 443
-  config.ignore   << "Releaf::AccessDenied"
-  config.ignore   << "ActiveRecord::StaleObjectError"
-  config.ignore   << "ActionController::ParameterMissing"
+  config.project_key  = '_error_report_key_'
+  config.project_id  = '_error_report_key_'
+  config.host         = 'https://office.cube.lv'
 end
+
+Airbrake.add_filter do |notice|
+  notice[:environment][:rails_environment] = ENV['RAILS_ENV']
+
+  ignoreable_errors = [
+    "Releaf::AccessDenied",
+    "ActiveRecord::StaleObjectError",
+    "ActionController::ParameterMissing"
+  ]
+
+  if notice[:errors].any?{|error| ignoreable_errors.include?(error[:type]) }
+    notice.ignore!
+  end
+end
+
