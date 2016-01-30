41454bd14a3b6abe77156bdb5bd28a36d774f02d
Miks Miķelsons <miks@cubesystems.lv>
2016-01-30 17:05:49 +0200

    Update releaf initializer

--- files/config/initializers/releaf.rb
+++ files/config/initializers/releaf.rb
@@ -1,26 +1,14 @@
 Releaf.application.configure do
-  # Default settings are commented out
-
-  ### setup menu items and therefore available controllers
   config.menu = [
     {
-      :name => "permissions",
-      :items =>   %w[releaf/permissions/users releaf/permissions/roles],
-      :icon => 'user',
+      name: "permissions",
+      items:   %w[releaf/permissions/users releaf/permissions/roles],
     },
     {
-      :controller => 'releaf/i18n_database/translations',
-      :icon => 'group',
+      controller: 'releaf/i18n_database/translations',
     },
   ]
 
-  # controllers that must be accessible by user, but are not visible in menu
-  # should be added to this list
-  config.additional_controllers = ['releaf/permissions/profile']
-
-  config.components = [Releaf::I18nDatabase, Releaf::Permissions]
-
+  config.components = [Releaf::Core, Releaf::I18nDatabase, Releaf::Permissions]
   config.available_locales = ["lv", "en"]
-  # conf.layout_builder_class_name = CustomLayoutBuilder
-  # conf.devise_for 'releaf/permissions/user'
 end
